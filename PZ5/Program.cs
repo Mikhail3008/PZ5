3c76d60037e43b3f6d6b7821b5438ad5e34a16c3
Ванюшкин Михаил <keitilotc11@gmail.com>
2023-04-17 11:19:35 +0700

    Добавьте файлы проекта.

---
+++ PZ5/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PZ5;
+
+Sfera sfera = new Sfera(5.0);
+Console.WriteLine("Радиус сферы:" + sfera.GetRadius());
---
+++ PZ5/Sfera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5
+{
+    public class Sfera
+    {
+        private double radius;
+        public Sfera(double radius)
+        {
+            this.radius = radius;
+        }
+        public double GetRadius()
+        {
+            return radius;
+        }
+
+    }
+
+}
